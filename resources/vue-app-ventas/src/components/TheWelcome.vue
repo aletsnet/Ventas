af7459d1f609d6fbeb791373bb338a79ac6588ac
Marco Garnica <marco.garnicah@outlook.com>
2023-08-07 23:59:03 -0600

    Integracion de front end VUE

---
+++ resources/vue-app-ventas/src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+	
+</script>
+
+<template>
+	<div class="container text-center">
+		<div class="row justify-content-center ">
+			<div class="col-3 shadow-sm bg-white">
+				<h1>Bienvenido</h1>
+			</div>
+		</div>
+	</div>
+</template>
